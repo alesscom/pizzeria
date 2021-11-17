ff4a73f5f3c4d13e5570c1cb613d4c61b37c8a0c
alesscom <90730443+alesscom@users.noreply.github.com>
2021-11-16 18:01:28 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,24 +4,22 @@ menu_list_array = ["Veg Margherita Pizza", "peperoni", "hawaiana", "familiar", "
 function getmenu(){
    var htmldata="";
    menu_list_array.sort();
-   for(var i=0;i<menu_list_array.lenght;i++){
+   for(var i=0;i<menu_list_array.length;i++){
     htmldata=htmldata + menu_list_array[i] + '<br>';
    }
    document.getElementById("display_menu").innerHTML=htmldata;
 }
-
 function add_item(){
 var htmldata;
-var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+var imgtags='<img id="im1" src="pizzaImg.png"/>'
 var item=document.getElementById("add_item").value;
 menu_list_array.sort();
 htmldata=""
-for(var i=0;i<menu_list_array.lenght;i++){
-    htmldata=htmldata + menu_list_array[i] + '<br>';
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata +imgtags+ menu_list_array[i] + '<br>';
     }
-document.getElementById("display_addmenu").innerHTML=htmldata;
+document.getElementById("display_added").innerHTML=htmldata;
 }
-
 function add_top(){
 var item=document.getElementById("add_item").value;
 menu_list_array.push(item);
